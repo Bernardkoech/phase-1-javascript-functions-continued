7e97c60ea3345280c4dc38a23221b8152a804f56
Bernard <bernard.koech@student.moringaschool.com>
2022-08-27 21:02:26 +0300

    commit tree b7d984b036c805e596111003d8c249865c84e393

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 // code your solution here
 function saturdayFun(activity="roller-skate") {
 
-    return `This Saturday, I want to ${activity}!`;
-
-  }
+  return `This Saturday, I want to ${activity}!`;
   
-  /*  defines mondayWork function expression as specified */
-
+  }
+  /* defines mondayWork function expression as specified */
+  
   let mondayWork = function(activity="go to the office") {
-
-    return `This Monday, I will ${activity}.`;
+  
+  return `This Monday, I will ${activity}.`;
   }
   /*defines wrapAdjective function according to the specification` */
-
   
   let wrapAdjective = function(style="*") {
-
-    return function(adjective="special") {
-
-      return `You are ${style}${adjective}${style}!`;
-
-    }
-    
+  
+  return function(adjective="special") {
+  
+  return `You are ${style}${adjective}${style}!`;
+  
   }
+  }
+   
+  
